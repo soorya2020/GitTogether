c75e3108dad0b7b506bd9b56b2174737384c37c0
soorya2020 <sooryakrishnanunni@gmail.com>
2025-11-28 20:55:38 +0530

    sokcetr io erro solved

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,9 +1,9 @@
 import { io } from "socket.io-client";
 
 export const createSocketConnection = () => {
-  // if (location.hostname === "localhost") {
-  //   return io( "http://localhost:4000");
-  // } else {
+  if (location.hostname === "localhost") {
+    return io( "http://localhost:4000");
+  } else {
     return io("/", { path: "/api/socket.io" });
-  // }
+  }
 };
